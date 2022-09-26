0bd4ead069b770f1724616f063235f1fb2ad97e9
erikbg7 <erik.bg7@gmail.com>
2022-09-26 10:11:16 +0200

    User atom and derived states

---
+++ src/atoms/user.ts
@@ -0,0 +1,20 @@
+import { atom } from 'jotai';
+import { focusAtom } from 'jotai/optics';
+
+interface User {
+  reward: number;
+  totalPoints: number;
+  userWords: string[];
+}
+
+const userAtom = atom<User>({
+  reward: 0,
+  totalPoints: 0,
+  userWords: [],
+});
+
+const rewardAtom = focusAtom(userAtom, (optic) => optic.prop('reward'));
+const totalPointsAtom = focusAtom(userAtom, (optic) => optic.prop('totalPoints'));
+const userWordsAtom = focusAtom(userAtom, (optic) => optic.prop('userWords'));
+
+export { rewardAtom, userWordsAtom, totalPointsAtom };
